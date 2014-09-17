f058680f998ea1ff05e2d9f063298030049a9226
Vaskar ROy <vaskar.jewel@gmail.com>
2014-09-17 14:05:52 +0600

    removed some code from iframe.php

--- iframe.php
+++ iframe.php
@@ -1,25 +1,11 @@
 <?php
 
-/**
- * @version $Id: mosiframe.php  2009-10-28 20:27
- * @ Viet Nguyen Hoang - viet4777 - www.xahoihoctap.net & www.luyenkim.net $
- * @package     Joomla
- * @subpackage Content
- * @copyright  Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
- * @license    GNU/GPL, see LICENSE.php
- * Joomla! is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- * See COPYRIGHT.php for copyright notices and details.
-
- */
 
 // Check to ensure this file is included in Joomla!
 
 defined('_JEXEC') or die();
 
-//jimport( 'joomla.event.plugin' );
+
 
 
 $enabled = JPluginHelper :: isEnabled  ('content','iframe');
